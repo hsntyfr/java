1671b75716e703e85a56d680ce245c39555e6bef
Hasan <hasantayfur69@hotmail.com>
2024-03-08 00:29:08 +0300

    first commit

---
+++ application1/src/application/main.java
@@ -0,0 +1,16 @@
+package application;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.print("İndirilecek repo url'sini giriniz: ");
+		Scanner input = new Scanner(System.in);
+		String url;
+		url = input.nextLine();
+		
+		
+
+	}
+
+}
